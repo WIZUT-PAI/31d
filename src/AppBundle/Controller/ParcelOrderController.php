03d86c324347f8cf521d29c5752a59a2b00373ac
Kamil Rywacki <krywacki@wizut.pl>
2016-05-04 14:41:13 +0200

    ParcelOrder-SendEmail-v2-working-json

--- src/AppBundle/Controller/ParcelOrderController.php
+++ src/AppBundle/Controller/ParcelOrderController.php
@@ -3,11 +3,13 @@
 namespace AppBundle\Controller;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 class ParcelOrderController extends FOSRestController
 {
     public function postParcelorderAction(Request $request) 
     {		
-	 var_dump($request->request->all()); 
-         $this->container->get('parcelorder_emailsender.handler')->sendEmail($request->request->all() );        
+	 #var_dump($request->request->all()); 
+        $this->container->get('parcelorder_emailsender.handler')->sendEmail($request->request->all() );        
+	return new Response("");
     }
 }
